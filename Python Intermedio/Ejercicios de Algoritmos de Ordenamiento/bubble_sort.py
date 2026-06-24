66034d3b993fa24ca7262c9491ac56412fcea3f0
Ard <ricardo.martinez@thomsonreuters.com>
2026-06-24 17:22:31 -0600

    correcting Sorting Algotithm

--- Python Intermedio/Ejercicios de Algoritmos de Ordenamiento/bubble_sort.py
+++ Python Intermedio/Ejercicios de Algoritmos de Ordenamiento/bubble_sort.py
@@ -1,15 +1,15 @@
 # Crea un bubble_sort por tu cuenta sin revisar el código de la lección.
 # Modifica el bubble_sort para que funcione de derecha a izquierda, ordenando los números menores primero (como en la imagen de abajo).
 
-def bubble_sort_right_to_left(list):
-    n = len(list)
-    print(f"Initial: {list}")
+def bubble_sort_right_to_left(arr):
+    n = len(arr)
+    print(f"Initial: {arr}")
     for i in range(n):
         for j in range(n - 1, i, -1):
-            if list[j] < list[j - 1]:
-                list[j], list[j - 1] = list[j - 1], list[j]
-                print(f"Pass {i + 1}, step {j}: {list}")
-    return list
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+                print(f"Pass {i + 1}, step {j}: {arr}")
+    return arr
 
-list = [64, 34, 25, 12, 22, 11, 90]
-print(f"Final:   {bubble_sort_right_to_left(list)}")
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(f"Final:   {bubble_sort_right_to_left(arr)}")
